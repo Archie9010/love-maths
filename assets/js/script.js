031bbdab1a8ba246ca5b75af5288d9d3669e4f57
Archie9010 <archie9010@protonmail.com>
2022-03-05 23:38:56 +0000

    Add multiplication code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,10 @@ function runGame(gameType){
 
     if (gameType === "addition") {
         displayAdditionQestion(num1, num2);
-    } else {
+    }else if (gameType === "multiply") {
+        displayMultiplyQestion(num1, num2);
+    } 
+    else {
         alert(`unknown game type : ${gameType}`);
         throw `unknown game type : ${gameType}. Aborting!`;
     }
@@ -73,7 +76,10 @@ function calculateCorrectAnswer(){
 
     if (operator === "+") {
         return [ operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [ operand1 * operand2, "multiply"];
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`; 
     }
@@ -109,6 +115,11 @@ function displaySubtractQestion(){
 
 }
 
-function displayMultiplyQestion(){
+function displayMultiplyQestion(operand1, operand2) {
+
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "x";
+ 
 
 }
